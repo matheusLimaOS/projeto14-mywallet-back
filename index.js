55b100401bbd3dc628331edde62af53b2245186a
matheussouzalima <matholaslima@gmail.com>
2022-11-21 15:34:39 -0300

    fix cors

--- index.js
+++ index.js
@@ -6,14 +6,9 @@ const app = express();
 
 
 app.use(express.json())
+app.use(cors());
+
 app.use(userRouter)
 app.use(transactionRouter)
 
-app.use((req,res,next)=>{
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,authorization");
-    app.use(cors());
-    next();
-})
-app.listen(process.env.PORT,console.log('On The Line'));
+app.listen(process.env.PORT || 3000,console.log('On The Line'));
