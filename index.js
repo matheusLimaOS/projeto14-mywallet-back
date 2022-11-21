9adb5bb493c84675c61e42322025d47948306952
matheussouzalima <matholaslima@gmail.com>
2022-11-21 15:50:11 -0300

    Criação rota status

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const app = express();
 
 app.use(express.json())
 app.use(cors());
-
+app.get("/status",(req,res)=>{
+    return res.send("OK");
+})
 app.use(userRouter)
 app.use(transactionRouter)
 
